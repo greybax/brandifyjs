df4df76cb51bb71ff2a0908cc2bb0103d3d1988f
Aleksandr Filatov <v-alfila@microsoft.com>
2017-05-09 16:17:33 -0700

    moved some functions to prepare.js

--- index.js
+++ index.js
@@ -34,33 +34,6 @@ module.exports = function brandify(defaults) {
 
   return api;
 
-  /**
-   * generate string with class and fill color for svg element
-   * 
-   * @param {string} name 
-   * @param {number} hex format color
-   * @returns {string}
-   */
-  function svgPatternReplacer(name, color) {
-    name = name
-      .toLowerCase()
-      .replace(' ', '');
-
-    return ` class="svg-brandify brandify-icon-${name}" fill="#${color}" `;
-  };
-
-  /**
-   * create new string with replacement string
-   * 
-   * @param {string} text 
-   * @param {number} position 
-   * @param {string} replacement 
-   * @returns {string}
-   */
-  function insertTo(text, position, replacement) {
-    return [text.slice(0, position), replacement, text.slice(position)].join('');
-  };
-
   function exec(text) {
     if (!text) {
       throw new Error('text should be initialized with not empty value');
@@ -68,7 +41,6 @@ module.exports = function brandify(defaults) {
 
     for (let i = 0; i < icons.length; i++) {
       const title = icons[i].title;
-      const color = icons[i].hex;
       const searchValue = new RegExp(`\\b(${title})\\b`, 'gi');
 
       if (title === 'Line') {
@@ -76,9 +48,7 @@ module.exports = function brandify(defaults) {
       }
 
       if (text.match(searchValue)) {
-        const replacer = svgPatternReplacer(title, color);
-        const svg = insertTo(icons[i].svg, '<svg'.length + 1, replacer);
-        text = text.replace(searchValue, svg);
+        text = text.replace(searchValue, icons[i].svg);
       }
     }
 
--- prepare.js
+++ prepare.js
@@ -1,14 +1,48 @@
 let icons = require('./jspm_packages/github/danleech/simple-icons@master/_data/simple-icons.json').icons;
 const fs = require('fs');
 
+
+
+/**
+ * generate string with class and fill color for svg element
+ * 
+ * @param {string} name 
+ * @param {number} hex format color
+ * @returns {string}
+ */
+function svgPatternReplacer(name, color) {
+  name = name
+    .toLowerCase()
+    .replace(' ', '');
+
+  return `class="svg-brandify brandify-icon-${name}" fill="#${color}" `;
+};
+
+/**
+ * create new string with replacement string
+ * 
+ * @param {string} text 
+ * @param {number} position 
+ * @param {string} replacement 
+ * @returns {string}
+ */
+function insertTo(text, position, replacement) {
+  return [text.slice(0, position), replacement, text.slice(position)].join('');
+};
+
 module.exports = function prepareSimpleIconsJson() {
   for (let i = 0; i < icons.length; i++) {
     let name = icons[i].title
       .toLowerCase()
       .replace(/ |[.]|[-]|[’]|[!]/g, '')
       .replace(/[+]/g, 'plus');
+    const color = icons[i].hex;
     const content = fs.readFileSync(`./jspm_packages/github/danleech/simple-icons@master/icons/${name}.svg`, 'utf8');
-    icons[i].svg = content;
+
+    const replacer = svgPatternReplacer(name, color);
+    const svg = insertTo(content, '<svg'.length + 1, replacer);
+    
+    icons[i].svg = svg;
   }
 
   fs.writeFileSync('./dist/simple-icons.json', `{"icons": ${JSON.stringify(icons)}}`, 'utf-8');
